58a8b79e43847a4751c7bc2bf525c94cb095de64
Abreezy70 <33820335+Abreezy70@users.noreply.github.com>
2018-09-05 12:37:48 -0400

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonExc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 241);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonExc
+            // 
+            this.buttonExc.Location = new System.Drawing.Point(434, 39);
+            this.buttonExc.Name = "buttonExc";
+            this.buttonExc.Size = new System.Drawing.Size(73, 23);
+            this.buttonExc.TabIndex = 1;
+            this.buttonExc.Text = "Execute Query";
+            this.buttonExc.UseVisualStyleBackColor = true;
+            this.buttonExc.Click += new System.EventHandler(this.buttonExc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 289);
+            this.Controls.Add(this.buttonExc);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonExc;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExc_Click(object sender, EventArgs e)
+        {
+            runQuery();
+        }
+
+        private void runQuery()
+        {
+            string query = textBox1.Text;
+            if(query == "")
+            {
+                MessageBox.Show("Please insert some sql query");
+                return;
+            }
+
+            string MySQLConnectionString = "datasource=ipaddress;port=3306;user=user;password=password;database=testing;SslMode = none";
+
+            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                if (myReader.HasRows)
+                {
+                    MessageBox.Show("Your query has generated results, see console.");
+
+                    while (myReader.Read())
+                    {
+                        Console.WriteLine(myReader.GetString(0) + " - " + myReader.GetString(1) + " - " + myReader.GetString(2) + " - " + myReader.GetString(3));
+                    }
+                } else
+                {
+                    MessageBox.Show("Query successfully executed");
+                }
+
+            } catch(Exception e)
+            {
+                MessageBox.Show("query error : " + e.Message);
+
+            }
+
+        }
+    }
+}
